46f357cd4dfcab0e1d2ac73eed28532b6143a53b
Fannerys Guardia Cuesta <guardiacuestafannerys@gmail.com>
2025-11-06 15:33:44 -0500

    feat  se crea ejemplo de lista

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,32 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //manza
+        String nombre="Manzana";
+        Integer precio=5000;
+        String descripcion="Deliciosa manzana importada";
+        Integer codigo=4587986;
+
+        //ARREGLO PRIMITIVO
+        String[] frutas= new String[500];
+
+        //ARREGLO EVOLUCIONADO (LISTA)
+        ArrayList<String> frutasDos= new ArrayList<>();
+        frutasDos.add("Manzana");
+        frutasDos.add("Pera");
+        frutasDos.add("Sandia");
+        System.out.println(frutasDos);
+
+        //Arreglo o lista de nombrees de clientes (5)--> String
+        //Arreglo olista de proveedores (10)--> String
+        // Arreglo o lista de valores de servicios publicos (5 meses) Integer/Double
+
+
     }
+
+
 }
