7003cfc5b9d6b6b5041976dda0f4245edbf24e03
Fannerys Guardia Cuesta <guardiacuestafannerys@gmail.com>
2025-11-07 17:53:17 -0500

    Termino el taller de ArrayList dejado en la clase virtual

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,8 +14,8 @@ public class Main {
         //ARREGLO PRIMITIVO
         String[] frutas= new String[500];
 
-        //ARREGLO EVOLUCIONADO (LISTA)
-        ArrayList<String> frutasDos= new ArrayList<>();
+        //ARREGLO EVOLUCIONADO DINAMICOS (LISTA)
+        ArrayList<String> frutasDos= new ArrayList<  >();
         frutasDos.add("Manzana");
         frutasDos.add("Pera");
         frutasDos.add("Sandia");
@@ -24,6 +24,38 @@ public class Main {
         //Arreglo o lista de nombrees de clientes (5)--> String
         //Arreglo olista de proveedores (10)--> String
         // Arreglo o lista de valores de servicios publicos (5 meses) Integer/Double
+
+        ArrayList<String> clientes= new ArrayList<>();
+        clientes.add("Fannerys");
+        clientes.add("Steven");
+        clientes.add("Mafe");
+        clientes.add("Adriana");
+        clientes.add("Cristian");
+        System.out.println(clientes);
+
+
+        ArrayList<String> proveedores=  new ArrayList<>();
+        proveedores.add("Sura");
+        proveedores.add("Éxito");
+        proveedores.add("Alkosto");
+        proveedores.add("Jumbo");
+        proveedores.add("D1");
+        proveedores.add("Ara");
+        proveedores.add("Homecenter");
+        proveedores.add("Falabella");
+        proveedores.add("Mercado Libre");
+        proveedores.add("Amazon");
+        System.out.println(proveedores);
+
+        ArrayList<Double> servicios = new ArrayList<>();
+        servicios.add(150000.50); // Mes 1
+        servicios.add(160200.00); // Mes 2
+        servicios.add(145000.75); // Mes 3
+        servicios.add(170000.00); // Mes 4
+        servicios.add(155500.25); // Mes 5
+        System.out.println("Servicios Publicos (5 meses): " + servicios);
+
+
 
 
     }
